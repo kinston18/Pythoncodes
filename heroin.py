017e39a2c60361122579a1378f9f9174f284d207
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2022-04-25 14:37:22 +0530

    Add files via upload

---
+++ heroin.py
@@ -0,0 +1,13 @@
+#S(S-a)(S-b)(S-c)
+#Area Calculation
+
+
+
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+s = (a + b + c) / 2
+
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %f' %area)
